1d1b082452561f5197237396cc3513b992f6c92d
周晶 <373087455@qq.com>
2019-11-09 20:11:20 +0800

    20191109

--- ApplicationCoreTest/UnitTest1.cs
+++ ApplicationCoreTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using Xunit;
 
 namespace ApplicationCoreTest
@@ -8,7 +9,27 @@ namespace ApplicationCoreTest
         [Fact]
         public void Test1()
         {
+            try
+            {
+                var dt = GetDataTable();
+            }
+            catch (Exception ex)
+            {
+                var es = ex;
+            }
+        }
 
+        private DataTable GetDataTable()
+        {
+            using (var dt = new DataTable())
+            {
+                dt.TableName = "test";
+                dt.Columns.Add("c1", typeof(string));
+                dt.Columns.Add("c2", typeof(string));
+                dt.Rows.Add("1", "1");
+                dt.Rows.Add("2", "2");
+                return dt;
+            }
         }
     }
 }
