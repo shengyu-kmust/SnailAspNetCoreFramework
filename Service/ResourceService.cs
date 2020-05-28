4735f4ca119d35bfad7a3fa807ddc60850d57df6
周晶 <373087455@qq.com>
2020-05-28 20:06:28 +0800

    增加资源管理功能

---
+++ Service/ResourceService.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entity;
+using ApplicationCore.IServices;
+using Snail.Core.Permission;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class ResourceService : BaseService<Resource>
+    {
+        public ResourceService(ServiceContext serviceContext) : base(serviceContext)
+        {
+        }
+    }
+}
---
+++ Web/Controllers/ResourceController.cs
@@ -0,0 +1,61 @@
+﻿using ApplicationCore.Entity;
+using ApplicationCore.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using Snail.Common;
+using Snail.Common.Extenssions;
+using Snail.Core;
+using Snail.Core.Permission;
+using System.Collections.Generic;
+using System.Linq;
+using Web.DTO;
+using Web.DTO.Resource;
+
+namespace Web.Controllers
+{
+
+    //[Authorize(Policy = PermissionConstant.PermissionAuthorizePolicy)]
+    public class ResourceController : DefaultBaseController, ICrudController<Resource, ResourceSaveDto, ResourceResultDto, KeyQueryDto>
+    {
+        private ResourceService _service;
+        private IPermissionStore _permissionStore;
+        public ResourceController(ResourceService service, ControllerContext controllerContext,IPermissionStore permissionStore, IPermission permission) : base(controllerContext)
+        {
+            this.controllerContext = controllerContext;
+            this._service = service;
+            this._permissionStore = permissionStore;
+        }
+        [HttpGet]
+        public List<ResourceResultDto> QueryList([FromQuery]KeyQueryDto queryDto)
+        {
+            var pred = ExpressionExtensions.True<Resource>().AndIf(queryDto.KeyWord.HasValue(), a => a.Name.Contains(queryDto.KeyWord) || a.Code.Contains(queryDto.KeyWord));
+            return controllerContext.mapper.ProjectTo<ResourceResultDto>(_service.QueryList(pred)).ToList();
+        }
+
+        [HttpGet]
+        public IPageResult<ResourceResultDto> QueryPage([FromQuery]KeyQueryDto queryDto)
+        {
+            var pred = ExpressionExtensions.True<Resource>().AndIf(queryDto.KeyWord.HasValue(), a => a.Name.Contains(queryDto.KeyWord) || a.Code.Contains(queryDto.KeyWord));
+            return controllerContext.mapper.ProjectTo<ResourceResultDto>(_service.QueryList(pred)).ToPageList(queryDto);
+        }
+        [HttpGet]
+        public ResourceResultDto Find(string id)
+        {
+            return controllerContext.mapper.Map<ResourceResultDto>(_service.QueryList(a => a.Id == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public void Remove(List<string> ids)
+        {
+            _service.Remove(ids);
+            _permissionStore.ReloadPemissionDatas();
+        }
+
+        [HttpPost]
+        public void Save(ResourceSaveDto saveDto)
+        {
+            _service.Save(saveDto);
+            _permissionStore.ReloadPemissionDatas();
+        }
+    }
+}
