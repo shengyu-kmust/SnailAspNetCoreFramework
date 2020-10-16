05ada0fa127e38aab57c723c836ffb3a6ca17213
周晶 <373087455@qq.com>
2020-10-16 15:32:18 +0800

    优化

---
+++ Web/AutoMapperProfiles/AutoMapperServiceCollectionExtenssion.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Web.AutoMapperProfiles
+{
+    public static class AutoMapperServiceCollectionExtenssion
+    {
+        public static void ConfigAutoMapper(this IServiceCollection services)
+        {
+            var assemblies = new List<Assembly>
+            {
+                Assembly.Load("Snail.Web"),
+                Assembly.Load("Web")
+            };
+            services.AddAutoMapper(assemblies);
+        }
+    }
+}
