d74d2d1fa09cc4116d791066d4a97080a59a31b2
周晶 <373087455@qq.com>
2020-10-23 17:05:39 +0800

    权限前端优化

--- Web/ClientApp/src/views/system/roleResource.vue
+++ Web/ClientApp/src/views/system/roleResource.vue
@@ -1,16 +1,27 @@
 <template>
   <div style="display:flex;flex:1">
     <el-row style="width:100%">
-      <el-col :span="12" style="height:100%">
+      <el-col :span="8" style="height:600px">
+        <el-input placeholder="搜索用户"></el-input>
         <snail-table
-          ref="table"
-          :rows="roles"
-          :fields="roleFields"
-          @row-click="selectRole"
+          ref="user"
+          :rows="users"
+          :fields="userFields"
+          @row-click="selectUser"
         ></snail-table>
       </el-col>
-      <el-col :span="12" style="height:100%">
-        <el-button @click="save">保存</el-button>
+      <el-col :span="8" style="height:600px">
+        <el-button @click="saveUserRole">设置人员角色</el-button>
+        <snail-table
+          ref="role"
+          :multiSelect='true'
+          :rows="roles"
+          :fields="roleFields"
+          @selection-change="selectRoles"
+        ></snail-table>
+      </el-col>
+      <el-col :span="8" style="height:600;overflow-y: scroll">
+        <el-button @click="saveRoleResource">设置角色权限</el-button>
         <el-tree
           ref="tree"
           :data="resources"
@@ -32,12 +43,15 @@ export default {
     return {
       resources: [], // 所有资源，树结构
       roleResources: [], // 记录角色的资源id
+      userRoles: [], // 记录人员的角色
       roles: [], // 所有角色列表
+      users: [], // 所有用户列表
       treeProps: {
         children: 'children',
         label: 'name'
       },
-      currentRow: {}
+      currentUserRow: {},
+      currentRoleRows:[]
     }
   },
   computed: {
@@ -49,30 +63,16 @@ export default {
         }
       ]
     },
-    fields() {
+    userFields() {
       return [
         {
           name: 'name',
           label: '姓名'
         },
-        {
+         {
           name: 'account',
           label: '账号'
         },
-        {
-          name: 'phone',
-          label: '电话'
-
-        },
-        {
-          name: 'email',
-          label: '邮箱'
-        },
-        {
-          name: 'gender',
-          label: '性别',
-          formatter: this.selectFormatter
-        }
       ]
     },
     searchFields() {
@@ -89,30 +89,80 @@ export default {
     this.init()
   },
   methods: {
-    save() {
-      var resourceKeys = this.$refs.tree.getCheckedKeys()
-      this.$api.setRoleResources({
-        roleKey: this.currentRow.id,
-        resourceKeys: resourceKeys
+    saveUserRole(){
+      debugger
+      var roleKeys = (this.currentRoleRows || []).map(v=>v.id);
+      this.$api.setUserRoles({
+        userKey: this.currentUserRow.id,
+        roleKeys: roleKeys
       }).then(res => {
         this.$message.success('保存成功')
-        this.selectRole(this.currentRow)
+        this.selectUser(this.currentUserRow)
       })
     },
-    selectRole(row) {
-      this.currentRow = row
-      this.$api.getRoleResources({ roleKey: row.id }).then(res => {
+    saveRoleResource() {
+      if(this.currentRoleRows.length==1){
+        var resourceKeys = this.$refs.tree.getCheckedKeys()
+        this.$api.setRoleResources({
+          roleKey: this.currentRoleRows[0].id,
+          resourceKeys: resourceKeys
+        }).then(res => {
+          this.$message.success('保存成功')
+          this.selectRole(this.currentRoleRows[0])
+        })
+      }else{
+        this.$$message.warn("一次只能对一个角色进行资源授权")
+      }
+      
+    },
+    selectRoles(section){
+      this.currentRoleRows=section;
+      console.log('currentRoleRows')
+      console.log(this.currentRoleRows)
+       if(section.length=1){
+        this.$api.getRoleResources({ roleKey: section[0].id }).then(res => {
         this.roleResources = res.data.resourceKeys || []
         this.$refs.tree.setCheckedKeys(this.roleResources)
       })
+      }
     },
-    init() {
-      this.$api.resourceQueryListTree().then(res => {
+    selectUser(row){
+      this.currentUserRow = row
+      this.$api.getUserRoles({ userKey: row.id }).then(res => {
+        this.userRoles = res.data.roleKeys || []
+        this.setRoleSelection(this.userRoles);
+      })
+    },
+    setRoleSelection(roleKeys){
+      if(roleKeys){
+        this.$refs.role.table.clearSelection();
+        var matchRoles=this.roles.filter(v=>roleKeys.findIndex(v.id)>-1);
+        console.log('matchRoles')
+        console.log(matchRoles);
+        matchRoles.forEach(role => {
+          this.$refs.role.table.toggleRowSelection(role,true)
+        });
+      }
+    },
+    getAllUser(){
+      this.$api.getAllUserInfo().then(res => {
+              this.users=res.data
+      })
+    },
+    getAllRole(){
+      this.$api.getAllRole().then(res => {
+              this.roles = res.data
+      })
+    },
+    getAllResource(){
+       this.$api.resourceQueryListTree().then(res => {
         this.resources = res.data
       })
-      this.$api.getAllRole().then(res => {
-        this.roles = res.data
-      })
+    },
+    init() {
+        this.getAllUser();
+        this.getAllRole();
+        this.getAllResource();
     }
   }
 }
