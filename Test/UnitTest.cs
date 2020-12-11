8072f83b3443dad6a299ec770e3260b115e6adbb
zhoujing <373087455@qq.com>
2020-12-11 23:00:31 +0800

    '优化'

--- Test/UnitTest.cs
+++ Test/UnitTest.cs
@@ -11,13 +11,7 @@ namespace Test
         {
             try
             {
-                "123".ConvertTo<int>();
-                "123.33".ConvertTo<double>();
-                "123.33".ConvertTo<decimal>();
-                "2012-01-01".ConvertTo<DateTime>();
-                "Monday".ConvertTo<DayOfWeek>();
-                123.ConvertTo<string>();
-                DayOfWeek.Monday.ConvertTo<string>();
+               
             }
             catch (Exception ex)
             {
