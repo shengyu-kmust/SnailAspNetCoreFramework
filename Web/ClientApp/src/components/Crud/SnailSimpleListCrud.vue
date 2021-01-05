769ec37df28078477c254a327b4e08f88a57b556
zhoujing <373087455@qq.com>
2021-01-05 22:57:14 +0800

    snailSimpleListCrud保存失败不关闭form表单

--- Web/ClientApp/src/components/Crud/SnailSimpleListCrud.vue
+++ Web/ClientApp/src/components/Crud/SnailSimpleListCrud.vue
@@ -19,17 +19,15 @@
 
     <div style="flex:1;">
       <!-- table分页 -->
-      <el-table
-        ref="table"
-        border
-        v-bind="tableBind"
-        :height="tableHeight"
-        :data="tableDatas"
-        :highlight-current-row="highlightCurrentRow"
-        @current-change="(currentRow)=>emitEventHandler('current-change',currentRow)"
-        @selection-change="(selecttion)=>emitEventHandler('selection-change',selecttion)"
-        @row-click="(row, column, event)=>emitEventHandler('row-click',row, column, event)"
-      >
+      <el-table ref="table"
+                border
+                v-bind="tableBind"
+                :height="tableHeight"
+                :data="tableDatas"
+                :highlight-current-row="highlightCurrentRow"
+                @current-change="(currentRow)=>emitEventHandler('current-change',currentRow)"
+                @selection-change="(selecttion)=>emitEventHandler('selection-change',selecttion)"
+                @row-click="(row, column, event)=>emitEventHandler('row-click',row, column, event)">
         <el-table-column v-if="multiSelect" type="selection"></el-table-column>
         <el-table-column v-if="showTableIndex" type="index" width="50">
           <template slot="header">
@@ -62,104 +60,113 @@
 </template>
 
 <script>
-import { TableBaseMixin } from '../Table/tableBase.js'
-export default {
-  mixins: [TableBaseMixin],
-  props: {
-    autoLoad: {
-      type: Boolean,
-      default: true
+  import { TableBaseMixin } from '../Table/tableBase.js'
+  export default {
+    mixins: [TableBaseMixin],
+    props: {
+      autoLoad: {
+        type: Boolean,
+        default: true
+      },
+      showOper: {
+        type: Boolean,
+        default: true
+      },
+      showSearch: {
+        type: Boolean,
+        default: true
+      },
+      searchApi: {
+        type: String,
+        default: () => ('')
+      },
+      addApi: {
+        type: String,
+        default: () => ('')
+      },
+      editApi: {
+        type: String,
+        default: () => ('')
+      },
+      removeApi: {
+        type: String,
+        default: () => ('')
+      },
+      searchFields: {
+        type: Array,
+        default: () => ([])
+      },
+      formFields: {
+        type: Array,
+        default: () => ([])
+      },
+      searchRules: {
+        type: Object,
+        default: () => ({})
+      },
+      formRules: {
+        type: Object,
+        default: () => ({})
+      },
+      beforeSearch: {
+        type: Function,
+        default: () => { }
+      },
+      afterSearch: {
+        type: Function,
+        default: () => { }
+      },
+      beforeSubmit: {
+        type: Function,
+        default: () => { }
+      },
+      handSearchTableDatas: {
+        type: Function,
+        default: null
+      },
+      tableBind: {
+        type: Object,
+        default: () => ({})
+      }
     },
-    showOper: {
-      type: Boolean,
-      default: true
+    data() {
+      return {
+        tableDatas: [],
+        submitApi: '',
+        formData: {},
+        visible: false,
+        loading: false
+      }
     },
-    showSearch: {
-      type: Boolean,
-      default: true
-    },
-    searchApi: {
-      type: String,
-      default: () => ('')
-    },
-    addApi: {
-      type: String,
-      default: () => ('')
-    },
-    editApi: {
-      type: String,
-      default: () => ('')
-    },
-    removeApi: {
-      type: String,
-      default: () => ('')
-    },
-    searchFields: {
-      type: Array,
-      default: () => ([])
-    },
-    formFields: {
-      type: Array,
-      default: () => ([])
-    },
-    searchRules: {
-      type: Object,
-      default: () => ({})
-    },
-    formRules: {
-      type: Object,
-      default: () => ({})
-    },
-    beforeSearch: {
-      type: Function,
-      default: () => { }
-    },
-    afterSearch: {
-      type: Function,
-      default: () => { }
-    },
-    beforeSubmit: {
-      type: Function,
-      default: () => { }
-    },
-    handSearchTableDatas: {
-      type: Function,
-      default: null
-    },
-    tableBind: {
-      type: Object,
-      default: () => ({})
-    }
-  },
-  data() {
-    return {
-      tableDatas: [],
-      submitApi: '',
-      formData: {},
-      visible: false,
-      loading: false
-    }
-  },
-  computed: {
+    computed: {
 
-  },
-  created() {
+    },
+    created() {
 
-  },
-  mounted() {
-    if (this.autoLoad) {
-      this.search()
-    }
-  },
-  methods: {
-    remove() {
-      var ids = []
-      var currentRow = this.currentRow
-      var selection = this.selection
-      console.log('删除-' + JSON.stringify(currentRow))
-      if (this.multiSelect) {
-        if (selection.length > 0) {
-          ids = selection.map(a => a.id)
+    },
+    mounted() {
+      if (this.autoLoad) {
+        this.search()
+      }
+    },
+    methods: {
+      remove() {
+        var ids = []
+        var currentRow = this.currentRow
+        var selection = this.selection
+        console.log('删除-' + JSON.stringify(currentRow))
+        if (this.multiSelect) {
+          if (selection.length > 0) {
+            ids = selection.map(a => a.id)
+          } else {
+            this.$message({
+              message: '请先选择要删除的数据',
+              type: 'warning'
+            })
+            return
+          }
+        } else if (currentRow.id) {
+          ids = [currentRow.id]
         } else {
           this.$message({
             message: '请先选择要删除的数据',
@@ -167,116 +174,103 @@ export default {
           })
           return
         }
-      } else if (currentRow.id) {
-        ids = [currentRow.id]
-      } else {
-        this.$message({
-          message: '请先选择要删除的数据',
-          type: 'warning'
-        })
-        return
-      }
-      this.$api[this.removeApi](ids).then(res => {
-        this.$message({
-          message: '操作成功',
-          type: 'success'
-        })
-        this.search()
-      }).catch(res => {
-        this.$message({
-          message: '操作失败',
-          type: 'error'
-        })
-      })
-    },
-    add() {
-      this.submitApi = this.addApi
-      this.formData = {}
-      this.visible = true
-    },
-    edit() {
-      if (this.multiSelect && this.selection.length > 1) {
-        this.$message({
-          message: '只能对一条数据进行修改',
-          type: 'warning'
-        })
-        return
-      }
-      if (!this.multiSelect && !this.currentRow.id) {
-        this.$message({
-          message: '请选择数据后再进行修改',
-          type: 'warning'
-        })
-        return
-      }
-      this.submitApi = this.editApi
-      this.formData = this.currentRow
-      this.visible = true
-    },
-    submit() {
-      this.$refs.form.validate(valid => {
-        if (valid) {
-          if (typeof this.beforeSubmit === 'function') {
-            var isContinue = this.beforeSubmit(this.$refs.form.formData)
-            if (isContinue === false) {
-              return
-            }
-          }
-          this.$api[this.submitApi](this.$refs.form.formData).then(res => {
-            this.$message({
-              message: '操作成功',
-              type: 'success'
-            })
-            this.search()
-          }).catch(res => {
-            this.$message({
-              message: '操作失败',
-              type: 'error'
-            })
+        this.$api[this.removeApi](ids).then(res => {
+          this.$message({
+            message: '操作成功',
+            type: 'success'
           })
-          this.visible = false
+          this.search()
+        }).catch(res => {
+          this.$message({
+            message: '操作失败',
+            type: 'error'
+          })
+        })
+      },
+      add() {
+        this.submitApi = this.addApi
+        this.formData = {}
+        this.visible = true
+      },
+      edit() {
+        if (this.multiSelect && this.selection.length > 1) {
+          this.$message({
+            message: '只能对一条数据进行修改',
+            type: 'warning'
+          })
+          return
         }
-      })
-    },
-    search() {
-      if (this.searchApi) {
-        this.$refs.searchForm.validate(valid => {
+        if (!this.multiSelect && !this.currentRow.id) {
+          this.$message({
+            message: '请选择数据后再进行修改',
+            type: 'warning'
+          })
+          return
+        }
+        this.submitApi = this.editApi
+        this.formData = this.currentRow
+        this.visible = true
+      },
+      submit() {
+        this.$refs.form.validate(valid => {
           if (valid) {
-            var serachForm = this.$refs.searchForm.formData
-            this.loading = true
-            if (typeof this.beforeSearch === 'function') {
-              var isContinue = this.beforeSearch(serachForm)
+            if (typeof this.beforeSubmit === 'function') {
+              var isContinue = this.beforeSubmit(this.$refs.form.formData)
               if (isContinue === false) {
                 return
               }
             }
-            this.$api[this.searchApi](serachForm).then(res => {
-              if (typeof this.handSearchTableDatas === 'function') {
-                var data = this.handSearchTableDatas(res)
-                if (data) {
-                  res.data = data
-                }
-              }
-
-              if (typeof this.afterSearch === 'function') {
-                this.afterSearch()
-              }
-              this.tableDatas = res.data
-            }).finally(() => {
-              this.loading = false
+            this.$api[this.submitApi](this.$refs.form.formData).then(res => {
+              this.$message({
+                message: '操作成功',
+                type: 'success'
+              })
+              this.visible = false
+              this.search()
             })
           }
         })
+      },
+      search() {
+        if (this.searchApi) {
+          this.$refs.searchForm.validate(valid => {
+            if (valid) {
+              var serachForm = this.$refs.searchForm.formData
+              this.loading = true
+              if (typeof this.beforeSearch === 'function') {
+                var isContinue = this.beforeSearch(serachForm)
+                if (isContinue === false) {
+                  return
+                }
+              }
+              this.$api[this.searchApi](serachForm).then(res => {
+                if (typeof this.handSearchTableDatas === 'function') {
+                  var data = this.handSearchTableDatas(res)
+                  if (data) {
+                    res.data = data
+                  }
+                }
+
+                if (typeof this.afterSearch === 'function') {
+                  this.afterSearch()
+                }
+                this.tableDatas = res.data
+              }).finally(() => {
+                this.loading = false
+              })
+            }
+          })
+        }
       }
     }
   }
-}
 </script>
-<style  lang="scss" scoped>
-.el-table{
-  /deep/ .el-table__body-wrapper{
+<style lang="scss" scoped>
+  .el-table {
+    /deep/ .el-table__body-wrapper
+
+  {
     overflow: scroll;
   }
-}
-
+  }
 </style>
